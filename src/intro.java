0f15e69d72135f7e64890fbd9c78d146064aefca
Otávio Lorenzo <otaviolorenzo8@gmail.com>
2025-11-17 22:48:26 -0300

    iniciando

---
+++ src/intro.java
@@ -0,0 +1,14 @@
+/* este projeto tem como objetivo usar IA para criar exercios onde posso praticar a linguagem JAVA
+e Git. Os exercicios são criado pelo Google Gemini e indicarei aqui o conteúdo
+
+<desafio1>
+
+Sistema de Vendas e Inventário Simples
+Crie um programa em Java para simular um mini-sistema de vendas que calcula o valor total
+de uma compra, aplica descontos e trata o inventário.
+
+</desafio1>
+
+*/
+
+
